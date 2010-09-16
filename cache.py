e587ea561888f5b7e8d0549eed8fa2c19b472ed5
giltayar <gil@tayar.org>
2010-09-16 21:38:06 +0200

    test_cache.py succeedsed

--- cache.py
+++ cache.py
@@ -13,3 +13,24 @@ the result depends only on the call's arguments.
 >> foo(2,3) # first call with args 2,3. calls wrapped function and remembers the result
 >> foo(1,2) # returns result from cache. doesn't call wrapped function.
 """
+
+import pickle
+
+def cache(f):
+    _cache = {}
+    def cached_function(*args, **kwds):
+        try:
+            key = (tuple(args), tuple(kwds.items()))
+            
+            if key in _cache:
+                return _cache[key]
+            else:
+                result = f(*args, **kwds)
+                _cache[key] = result
+                return result
+        except pickle.PicklingError:
+            return f(*args, **kwds)
+    
+    return cached_function
+
+    
