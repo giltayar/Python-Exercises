253ef61916104dc59986708107b0e2901321cfc0
Gil Tayar <gil@tayar.org>
2010-10-07 21:31:01 +0200

    factory implemented and running unit tests successfully

--- factory.py
+++ factory.py
@@ -6,3 +6,17 @@ A generic factory that creates objects for "roles" with registered creation func
 >> ...
 >> a = f.create('A',1,2,x=3) # create object of role 'A' and pass the other arguments to it's constructor
 """
+########################################################################
+class Factory(object):
+    def __init__(self):
+        self.registered_interfaces = {}
+        
+    def register(self, role_name, implementing_class):
+        self.registered_interfaces[role_name] = implementing_class
+        
+    def create(self, role_name, *args, **kwds):
+        return self.registered_interfaces[role_name](*args, **kwds)
+        
+        
+    
+    
