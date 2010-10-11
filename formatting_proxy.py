ce851d5028e8b5ac019fdd855820e038d4ce62f2
Gil Tayar <gil@tayar.org>
2010-10-11 12:48:33 +0200

    formatting_proxy.py written and unit-tested

--- formatting_proxy.py
+++ formatting_proxy.py
@@ -8,3 +8,35 @@ format.
 
 See the unit tests for more details on how this behaves.
 """
+from UserDict import UserDict
+import string
+
+class FormattingProxy(dict):
+    def st_items_retrieved(self):
+        pass
+    
+    def __init__(self, data):
+        self.update(data)
+        self.st_items_retrieved = set()
+        
+    def __getitem__(self, key):
+        
+        if key in self:
+            self.st_items_retrieved.add(key)
+            return dict.__getitem__(self, key)
+        else:
+            return "<UNKNOWN PARAMETER " + str(key) + ">"
+    
+def format(fmt, **kwds):
+    formatting_proxy = FormattingProxy(kwds)
+    ret = fmt % formatting_proxy
+    
+    additional_arguments = set(kwds.iterkeys()).difference(formatting_proxy.st_items_retrieved)
+    
+    if (len(additional_arguments) > 0):
+        return ret + "\nAdditional arguments:\n" + \
+               "\n".join([(arg + " = " + str(kwds[arg])) for arg in sorted(additional_arguments)]) + "\n"
+    else:
+        return ret
+           
+    
