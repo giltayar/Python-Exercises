722d854f7d51030c21cfcbfb5435205ecf72082d
Gil Tayar <gil@tayar.org>
2010-10-18 16:56:33 +0200

    partial works, with unit tests

--- partial.py
+++ partial.py
@@ -8,3 +8,14 @@ with only the remaining arguments left.
 >> f = partial(foo,1,z=3) # this binds x=1 (positional) and z=3 (by name)
 >> f(2) # equivalent to foo(1,2,3)
 """
+
+from functools import wraps
+
+def partial(function, *partial_args, **partial_kwds):
+    @wraps(function)
+    def wrapper(*args, **kwds):
+        final_kwds = dict(partial_kwds)
+        final_kwds.update(kwds)
+        return function(*(partial_args + args), **final_kwds)
+    
+    return wrapper
