b29c04f3575171f506163a0a653c31b45dc07712
Gil Tayar <gil@tayar.org>
2010-10-18 18:16:46 +0200

    Tracer.py works with unit tests

--- tracer.py
+++ tracer.py
@@ -7,3 +7,20 @@ Depends on DynamicProxy.
 >> tracer.foo(...) # calls x.foo, and sends trace of call and return value to the logger
 
 """
+from dynamic_proxy import DynamicProxy
+
+class Tracer(DynamicProxy):
+    def __init__(self, object, logger):
+        self.object = object
+        self.logger = logger
+        
+    def _dispatch(self, function_name, *args, **kwds):
+        self.logger("%s called with a=%r, kw=%r" % (function_name, args, kwds))
+        
+        ret = getattr(self.object, function_name)(*args, **kwds)
+        
+        self.logger("%s returned %s" % (function_name, ret))
+        
+        return ret
+    
+    
